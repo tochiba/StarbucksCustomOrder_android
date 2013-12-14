1529217fe7acd92415ae7299f3d0ef4c91e8e4fb
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2013-12-14 23:57:20 +0900

    import文の整理

--- StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/view/like/matrix/MatrixSelectorLayout.java
+++ StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/view/like/matrix/MatrixSelectorLayout.java
@@ -1,7 +1,6 @@
 package com.koganepj.starbuckscustomorder.view.like.matrix;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
