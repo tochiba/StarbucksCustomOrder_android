dd567f80921efb1b5fc95e38e77fbef4a1edb203
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2013-12-21 13:05:23 +0900

    スターバックスラテの仮表示

--- StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/view/menu/visual/VisualAdapter.java
+++ StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/view/menu/visual/VisualAdapter.java
@@ -1,22 +1,25 @@
 package com.koganepj.starbuckscustomorder.view.menu.visual;
 
+import com.koganepj.starbuckscustomorder.R;
+
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 public class VisualAdapter extends ArrayAdapter<String> {
     
-    //private LayoutInflater mInflater;
+    private LayoutInflater mInflater;
     
     public VisualAdapter(Context context) {
-        super(context, android.R.layout.simple_list_item_1);
-        //mInflater = LayoutInflater.from(context);
+        super(context, 0);
+        mInflater = LayoutInflater.from(context);
     }
     
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        return mInflater.inflate(R.layout.layout_menu_visual_cell, null);
     }
 
 }
