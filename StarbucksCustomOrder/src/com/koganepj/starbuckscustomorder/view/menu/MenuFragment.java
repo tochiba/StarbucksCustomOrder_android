216c99da1095a1e81f71a75175722cd8db522988
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2013-12-21 22:14:14 +0900

    リスト表示のレイアウトを分けて微調整

--- StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/view/menu/MenuFragment.java
+++ StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/view/menu/MenuFragment.java
@@ -1,13 +1,12 @@
 package com.koganepj.starbuckscustomorder.view.menu;
 
 import android.app.Fragment;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
@@ -19,36 +18,45 @@ import com.koganepj.starbuckscustomorder.view.menu.visual.VisualAdapter;
 public class MenuFragment extends Fragment {
     
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_menu, null);
 
-        final ListView listView = (ListView)view.findViewById(R.id.ListMenu);
+//        final ListView listView = (ListView)view.findViewById(R.id.ListMenu);
+        final FrameLayout frameLayout = (FrameLayout)view.findViewById(R.id.ListFrame);
         RadioGroup modeRadioGroup = (RadioGroup)view.findViewById(R.id.RadioGroupMenuShowType);
         
         //モード切り替えを仮実装
         modeRadioGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (checkedId == R.id.RadioSimple) {
+                if (checkedId == R.id.RadioSimple) {//シンプルモード
+                    //ListViewの入れ替え
+                    frameLayout.removeAllViews();
+                    ListView listView = (ListView)inflater.inflate(R.layout.layout_menu_list_simple, null);
+                    frameLayout.addView(listView);
+                    
                     ArrayAdapter<String> arrayAdapter = new SimpleAdapter(getActivity());
                     for (int i = 0; i < 100; i++) {
                         arrayAdapter.add("count : " + i);
                     }
                     listView.setAdapter(arrayAdapter);
-                    listView.setDivider(new ColorDrawable(getResources().getColor(android.R.color.black)));
-                    listView.setDividerHeight(1);
                     return;
                 }
                 
-                if (checkedId == R.id.RadioVisual) {
+                if (checkedId == R.id.RadioVisual) {//ビジュアルモード
+                    //ListViewの入れ替え
+                    frameLayout.removeAllViews();
+                    ListView listView = (ListView)inflater.inflate(R.layout.layout_menu_list_visual, null);
+                    frameLayout.addView(listView);
+
                     //仮データを入れておく
                     ArrayAdapter<String> adapter = new VisualAdapter(getActivity());
                     listView.setAdapter(adapter);
                     for (int i = 0; i < 100; i++) {
                         adapter.add("number:" + (i + 1) + " (from visualadapter)");
                     }
-                    listView.setDivider(null);
                     listView.setAdapter(adapter);
+                    return;
                 }
             }
         });
