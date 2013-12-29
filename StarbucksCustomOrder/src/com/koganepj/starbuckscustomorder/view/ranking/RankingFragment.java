5eb8737c8e6147ba8b421f4b9619fdd9efc5c424
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2013-12-29 11:51:40 +0900

    価格表示とカロリー表示を実装

--- StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/view/ranking/RankingFragment.java
+++ StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/view/ranking/RankingFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.RadioGroup;
 
 import com.koganepj.starbuckscustomorder.R;
 import com.koganepj.starbuckscustomorder.model.SimpleCoffeeModel;
@@ -20,13 +21,21 @@ public class RankingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_ranking, null);
         
+        //リストを読み込み
         CoffeeListParser parser = new CoffeeListParser(getActivity());
         ArrayList<SimpleCoffeeModel> coffeeList = parser.getCoffeeList();
-
+        
+        //アダプターをセット
         ListView listView = (ListView)view.findViewById(R.id.ListMenu);
         RankingAdapter adapter = new RankingAdapter(getActivity());
-        adapter.addAll(coffeeList);
         listView.setAdapter(adapter);
+        
+        //ラジオボタンの動作設定
+        RadioGroup radioGroup = (RadioGroup)view.findViewById(R.id.RadioGroupMenuShowType);
+        radioGroup.setOnCheckedChangeListener(new TypeSelectListener(adapter, coffeeList));
+        
+        //初期表示
+        radioGroup.check(R.id.RadioPrice);
         
         return view;
     }
---
+++ StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/view/ranking/TypeSelectListener.java
@@ -0,0 +1,51 @@
+package com.koganepj.starbuckscustomorder.view.ranking;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+
+import com.koganepj.starbuckscustomorder.R;
+import com.koganepj.starbuckscustomorder.model.SimpleCoffeeModel;
+import com.koganepj.starbuckscustomorder.view.ranking.adapter.RankingAdapter;
+
+public class TypeSelectListener implements OnCheckedChangeListener {
+    
+    private RankingAdapter mAdapter;
+    private ArrayList<SimpleCoffeeModel> mCoffeeModels;
+    
+    public TypeSelectListener(RankingAdapter adapter, ArrayList<SimpleCoffeeModel> coffeeModels) {
+        mAdapter = adapter;
+        mCoffeeModels = coffeeModels;
+    }
+    
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        if (checkedId == R.id.RadioCalorie) {
+            mAdapter.clear();
+            
+            ArrayList<SimpleCoffeeModel> copiedList = new ArrayList<SimpleCoffeeModel>(mCoffeeModels);
+            Collections.sort(copiedList, new CalorieComparator());
+            
+            mAdapter.addAll(copiedList);
+            return;
+        }
+        
+        if (checkedId == R.id.RadioPrice) {
+            mAdapter.clear();
+            
+            ArrayList<SimpleCoffeeModel> copiedList = new ArrayList<SimpleCoffeeModel>(mCoffeeModels);
+            Collections.sort(copiedList, new PriceComparator());
+            
+            mAdapter.addAll(copiedList);
+            return;
+        }
+
+        if (checkedId == R.id.RadioSocial) {
+
+            return;
+        }
+    }
+    
+}
