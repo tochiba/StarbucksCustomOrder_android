fb4ec5294bbea1172755d82ddc12e847b071273e
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2013-12-30 22:48:52 +0900

    トッピングアイコンのクリックイベントを仮作成

--- StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/custom/CustomFragment.java
+++ StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/custom/CustomFragment.java
@@ -1,18 +1,30 @@
 package com.koganepj.starbuckscustomorder.custom;
 
-import com.koganepj.starbuckscustomorder.R;
-
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.koganepj.starbuckscustomorder.R;
 
 public class CustomFragment extends Fragment {
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_custom, null);
+        View view = inflater.inflate(R.layout.fragment_custom, null);
+        
+        View toppingView = view.findViewById(R.id.ImageTopping);
+        toppingView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Let's Topping!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        return view;
     }
     
 }
