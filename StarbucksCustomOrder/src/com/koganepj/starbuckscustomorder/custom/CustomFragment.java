fd04dca45e4ea2ce161138e18dda9a41e8b16ce8
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2013-12-30 15:05:54 +0900

    静的にレイアウトを配置

--- StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/custom/CustomFragment.java
+++ StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/custom/CustomFragment.java
@@ -1,7 +1,18 @@
 package com.koganepj.starbuckscustomorder.custom;
 
+import com.koganepj.starbuckscustomorder.R;
+
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class CustomFragment extends Fragment {
-
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_custom, null);
+    }
+    
 }
