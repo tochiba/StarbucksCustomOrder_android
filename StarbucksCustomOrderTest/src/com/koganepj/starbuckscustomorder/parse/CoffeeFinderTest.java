b07d20fe5e2635c9bbc3a34a761d304737e8a9cf
alk-tnk <alk.tnk@gmail.com>
2013-12-31 15:35:41 +0900

    テストコードの指摘箇所を修正

--- StarbucksCustomOrderTest/src/com/koganepj/starbuckscustomorder/parse/CoffeeFinderTest.java
+++ StarbucksCustomOrderTest/src/com/koganepj/starbuckscustomorder/parse/CoffeeFinderTest.java
@@ -14,26 +14,6 @@ import com.koganepj.starbuckscustomorder.model.Sweetness;
 
 public class CoffeeFinderTest extends AndroidTestCase {
     
-    public void test甘さと温度にそれぞれ５段階づつの幅がある() {
-        CoffeeNameFinder finder = new CoffeeNameFinder(getContext());
-        
-        //甘さと温度の５段階ずつをテスト
-        for (int sweetnessValue = 0; sweetnessValue < 5; sweetnessValue++) {
-            for (int hotnessValue = 0; hotnessValue < 5; hotnessValue++) {
-                Sweetness sweetness = new Sweetness(sweetnessValue);
-                Hotness hotness = new Hotness(hotnessValue);
-                
-                //検査
-                CoffeeName coffeeName = finder.findName(sweetness, hotness);
-                String errorMessage = String.format(Locale.getDefault(), "甘さ：%d、温度：%d がありません", sweetness.getLevel(), hotness.getLevel());
-                assertNotNull(errorMessage, coffeeName);
-                
-                //一応コーヒーの名前を出力して確認しておく
-                Log.d("test", "The coffeeName is " + coffeeName.getCoffeeName());
-            }
-        }
-    }
-    
     public void test商品一覧から詳細情報を取得する() {
     	CoffeeListParser parser = new CoffeeListParser(getContext());
     	ArrayList<SimpleCoffeeModel> coffeeLists = parser.getCoffeeList();
@@ -42,9 +22,9 @@ public class CoffeeFinderTest extends AndroidTestCase {
             CoffeeFinder finder = new CoffeeFinder(getContext(), model.coffeeName);
             Coffee coffee = finder.find();
             
-			String errorMessage = String.format(Locale.getDefault(), "%s が見つかりません", model.coffeeName.getCoffeeName());
-			// 値段で取得できたか確認する
-			assertNotNull(errorMessage, coffee.price);
+            String errorMessage = String.format(Locale.getDefault(), "%s が見つかりません", model.coffeeName.getCoffeeName());
+            // 値段で取得できたか確認する
+            assertNotNull(errorMessage, coffee.price);
                 
             //一応コーヒーの名前を出力して確認しておく
             Log.d("test", "The coffeeName is " + coffee.name);
@@ -72,7 +52,5 @@ public class CoffeeFinderTest extends AndroidTestCase {
                 Log.d("test", "The coffeeName is " + coffee.name);
             }
         }
-    	
     }
-    
 }
