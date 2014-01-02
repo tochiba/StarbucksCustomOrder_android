2fa1ea59e4a8f7288128d1896d7749b3adcc09b5
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2014-01-03 00:49:14 +0900

    パラメータの受け渡しを仮実装

--- StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/custom/CustomActivity.java
+++ StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/custom/CustomActivity.java
@@ -1,8 +1,8 @@
 package com.koganepj.starbuckscustomorder.custom;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.koganepj.starbuckscustomorder.R;
 import com.koganepj.starbuckscustomorder.model.CoffeeName;
@@ -16,9 +16,12 @@ public class CustomActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom);
         
-        //パラメータを取得
+        //パラメータを取得してFragmentに渡す
         CoffeeName coffeeName = (CoffeeName)getIntent().getSerializableExtra(KEY_PARAM_COFFEENAME);
-        Log.d("test", "The coffee name is " + coffeeName.getCoffeeName());
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(CustomFragment.KEY_PARAM_COFFEENAME, coffeeName);
+        Fragment fragment = getFragmentManager().findFragmentById(R.id.FragmentCustom);
+        fragment.setArguments(bundle);
         
         findViewById(R.id.TextBack).setOnClickListener(new BackTextClickListener(this));
     }
--- StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/custom/CustomFragment.java
+++ StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/custom/CustomFragment.java
@@ -2,14 +2,18 @@ package com.koganepj.starbuckscustomorder.custom;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.koganepj.starbuckscustomorder.R;
 import com.koganepj.starbuckscustomorder.custom.animation.OnShowSelectToppingViewListener;
+import com.koganepj.starbuckscustomorder.model.CoffeeName;
 
 public class CustomFragment extends Fragment {
+    
+    public static final String KEY_PARAM_COFFEENAME = "key_coffeename";
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -20,7 +24,11 @@ public class CustomFragment extends Fragment {
         View infoView = view.findViewById(R.id.LayoutInfo);
         View imageView = view.findViewById(R.id.LayoutImage);
         
-        //トッピング開始
+        //パラメータを受け取って表示要素の設定
+//        CoffeeName coffeeName = (CoffeeName)getArguments().getSerializable(KEY_PARAM_COFFEENAME);
+//        Log.d("test", "fragment get coffe name : " + coffeeName.getCoffeeName());
+        
+        //トッピング開始時のアニメーション設定
         View toppingView = view.findViewById(R.id.ImageTopping);
         toppingView.setOnClickListener(new OnShowSelectToppingViewListener(toppingSelectView, sizeSelectView, infoView, imageView));
         
