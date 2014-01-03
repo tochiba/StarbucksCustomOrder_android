ad100b8f3612f0329daad8801c15cd33556e58b6
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2014-01-03 09:49:17 +0900

    温度選択用のカスタムビューを作成

---
+++ StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/custom/view/HotnessSelectView.java
@@ -0,0 +1,13 @@
+package com.koganepj.starbuckscustomorder.custom.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Switch;
+
+public class HotnessSelectView extends Switch {
+
+    public HotnessSelectView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
