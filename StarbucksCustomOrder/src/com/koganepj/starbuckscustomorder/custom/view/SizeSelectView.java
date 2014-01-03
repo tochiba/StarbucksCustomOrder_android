56d7949167c8a2b3ed55c11a5bcdf9c7d8e2f55e
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2014-01-04 00:44:20 +0900

    サイズ選択用のカスタムレイアウトのひな形を作成

---
+++ StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/custom/view/SizeSelectView.java
@@ -0,0 +1,13 @@
+package com.koganepj.starbuckscustomorder.custom.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class SizeSelectView extends LinearLayout {
+
+    public SizeSelectView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
