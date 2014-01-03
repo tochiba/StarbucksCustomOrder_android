8c43af6ae0f9d5d158ea5ea5aaa931d8489e39f5
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2014-01-03 15:29:43 +0900

    選択なしの場合の表示を実装

--- StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/custom/view/TempuretureSelectView.java
+++ StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/custom/view/TempuretureSelectView.java
@@ -4,17 +4,25 @@ import java.util.ArrayList;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.widget.Switch;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
-public class TempuretureSelectView extends Switch {
+import com.koganepj.starbuckscustomorder.R;
+
+public class TempuretureSelectView extends FrameLayout {
     
     public TempuretureSelectView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
     
     public void setTempureture(ArrayList<String> tempuretures) {
-        Log.d("test", "tempureture : " + tempuretures);
+        //選択なしの表示
+        LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.layout_custom_tempureture_select_single, this);
+        
+        TextView textView = (TextView)getChildAt(0);
+        textView.setText(tempuretures.get(0));
     }
     
 }
