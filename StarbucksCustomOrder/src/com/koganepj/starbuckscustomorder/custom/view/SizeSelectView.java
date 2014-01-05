b978f1fb85ed37893a53ead7c46bc560e68ca7ce
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2014-01-05 11:34:47 +0900

    表示位置を調整

--- StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/custom/view/SizeSelectView.java
+++ StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/custom/view/SizeSelectView.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -23,14 +24,23 @@ public class SizeSelectView extends RadioGroup {
     public void setSize(ArrayList<String> sizeList) {
         LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         
+        addView(createSpaceView());
+        
         for (String sizeStr : sizeList) {
             RadioButton radioButton = (RadioButton)inflater.inflate(R.layout.layout_custom_size_select_radio, null);
             radioButton.setId(mSelectSizeMapper.getId(sizeStr));
             radioButton.setBackgroundResource(mSelectSizeMapper.getSelector(sizeStr));
             addView(radioButton);
+            addView(createSpaceView());
         }
         
-        ((RadioButton)getChildAt(0)).setChecked(true);
+        ((RadioButton)getChildAt(1)).setChecked(true);
+    }
+    
+    View createSpaceView() {
+        View spaceView = new View(getContext());
+        spaceView.setLayoutParams(new LayoutParams(0, 0, 1F));
+        return spaceView;
     }
     
 
