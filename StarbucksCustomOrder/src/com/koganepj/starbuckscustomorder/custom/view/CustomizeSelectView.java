6681f6c8dc3a81db58d335aec88920874152be0a
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2014-01-05 11:45:20 +0900

    カスタマイズ用のカスタムレイアウトを作成

---
+++ StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/custom/view/CustomizeSelectView.java
@@ -0,0 +1,13 @@
+package com.koganepj.starbuckscustomorder.custom.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+public class CustomizeSelectView extends FrameLayout {
+
+    public CustomizeSelectView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
