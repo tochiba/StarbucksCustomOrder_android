30b5ee284f508987e1843289b3e590f5ab40f51e
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2014-01-05 21:47:59 +0900

    カスタマイズ項目の表示を仮実装

--- StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/custom/view/CustomizeSelectView.java
+++ StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/custom/view/CustomizeSelectView.java
@@ -3,7 +3,10 @@ package com.koganepj.starbuckscustomorder.custom.view;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.GridLayout;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.koganepj.starbuckscustomorder.R;
 import com.koganepj.starbuckscustomorder.model.Coffee;
@@ -18,7 +21,55 @@ public class CustomizeSelectView extends LinearLayout {
     }
     
     public void setCoffeeToCreateView(Coffee coffee) {
+        GridLayout grid = (GridLayout)findViewById(R.id.GridLayout);
         
+        if (coffee.base.size() != 0) {
+            View view = LayoutInflater.from(getContext()).inflate(R.layout.layout_custom_customize_select, null);
+            ((TextView)view.findViewById(R.id.TextCustomizeDetail)).setText("Base");
+            grid.addView(view);
+        }
+        
+        if (coffee.espresso.size() != 0) {
+            View view = LayoutInflater.from(getContext()).inflate(R.layout.layout_custom_customize_select, null);
+            ((TextView)view.findViewById(R.id.TextCustomizeDetail)).setText("Espresso");
+            grid.addView(view);
+        }
+
+        if (coffee.jelly.size() != 0) {
+            View view = LayoutInflater.from(getContext()).inflate(R.layout.layout_custom_customize_select, null);
+            ((TextView)view.findViewById(R.id.TextCustomizeDetail)).setText("Jelly");
+            grid.addView(view);
+        }
+
+        if (coffee.milk.size() != 0) {
+            View view = LayoutInflater.from(getContext()).inflate(R.layout.layout_custom_customize_select, null);
+            ((TextView)view.findViewById(R.id.TextCustomizeDetail)).setText("Milk");
+            grid.addView(view);
+        }
+
+        if (coffee.powder.size() != 0) {
+            View view = LayoutInflater.from(getContext()).inflate(R.layout.layout_custom_customize_select, null);
+            ((TextView)view.findViewById(R.id.TextCustomizeDetail)).setText("Powder");
+            grid.addView(view);
+        }
+
+        if (coffee.sauce.size() != 0) {
+            View view = LayoutInflater.from(getContext()).inflate(R.layout.layout_custom_customize_select, null);
+            ((TextView)view.findViewById(R.id.TextCustomizeDetail)).setText("Sauce");
+            grid.addView(view);
+        }
+
+        if (coffee.syrup.size() != 0) {
+            View view = LayoutInflater.from(getContext()).inflate(R.layout.layout_custom_customize_select, null);
+            ((TextView)view.findViewById(R.id.TextCustomizeDetail)).setText("Syrup");
+            grid.addView(view);
+        }
+
+        if (coffee.whippedCream.size() != 0) {
+            View view = LayoutInflater.from(getContext()).inflate(R.layout.layout_custom_customize_select, null);
+            ((TextView)view.findViewById(R.id.TextCustomizeDetail)).setText("WhippedCream");
+            grid.addView(view);
+        }
     }
     
 }
