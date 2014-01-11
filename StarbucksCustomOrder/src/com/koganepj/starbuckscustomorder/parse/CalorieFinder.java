085e5ea058f5a70cc88d219b0a9d9a3d05659294
alk-tnk <alk.tnk@gmail.com>
2014-01-11 23:34:54 +0900

    コーヒーのサイズに合わせてカロリーを返す処理を追加

--- StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/parse/CalorieFinder.java
+++ StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/parse/CalorieFinder.java
@@ -1,28 +1,54 @@
 package com.koganepj.starbuckscustomorder.parse;
 
+import java.util.HashMap;
+
 import android.content.Context;
 
 import com.dd.plist.NSDictionary;
 import com.koganepj.starbuckscustomorder.model.Calorie;
+import com.koganepj.starbuckscustomorder.model.Coffee;
+import com.koganepj.starbuckscustomorder.model.Size;
 
 public class CalorieFinder {
 	private static final String CALORIE_DICT = "CalorieDic";
-	Context mContext;
-
+	private static final HashMap<String, Integer> COFFEE_VOLUME = new HashMap<String, Integer>(); 
+	static {
+		COFFEE_VOLUME.put("Short", 240);
+		COFFEE_VOLUME.put("Tall", 350);
+		COFFEE_VOLUME.put("Grande", 470);
+		COFFEE_VOLUME.put("Venti", 590);
+		COFFEE_VOLUME.put("ワンサイズ", -1);
+	}
+	
+	private NSDictionary mCalorieDict;
+	
 	public CalorieFinder(Context context) {
-		mContext = context;
+		mCalorieDict = getCalorieDict(context);
+		
 	}
 
 	public Calorie getCalorie(String name) {
-		final NSDictionary calorieDict = getCalorieDict();
-		final int value = NSDictionaryHelper.intForKey(calorieDict, name);
+		final int value = NSDictionaryHelper.intForKey(mCalorieDict, name);
 
 		return new Calorie(value);
 	}
+	
+	public Calorie getCoffeeSizeCalorie(Coffee coffee, Size currentSize) {
+		// 選択されたコーヒーの容量を取得
+		int currentValue = COFFEE_VOLUME.get(currentSize.getSize());
+		if (currentValue == -1) {
+			// ワンサイズの場合は、カロリーをそのまま返す
+			return new Calorie(coffee.calorie.getCalorie());
+		}
+		
+		// 1mlあたりのカロリーを取得
+		double caloriePerValue = ((double)coffee.calorie.getCalorie()) / ((double)COFFEE_VOLUME.get(coffee.size.get(0).getSize()));
+		return new Calorie((int)(currentValue * caloriePerValue));
+	}
 
-	private NSDictionary getCalorieDict() {
+	NSDictionary getCalorieDict(Context context) {
 		final NSDictionary rootDict = NSDictionaryHelper
-				.returnRootDictionary(mContext);
+				.returnRootDictionary(context);
 		return (NSDictionary) (rootDict.objectForKey(CALORIE_DICT));
 	}
 }
--- StarbucksCustomOrderTest/src/com/koganepj/starbuckscustomorder/parse/CalorieFinderTest.java
+++ StarbucksCustomOrderTest/src/com/koganepj/starbuckscustomorder/parse/CalorieFinderTest.java
@@ -1,5 +1,7 @@
 package com.koganepj.starbuckscustomorder.parse;
 
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Map.Entry;
 
 import android.test.AndroidTestCase;
@@ -8,6 +10,9 @@ import android.util.Log;
 import com.dd.plist.NSDictionary;
 import com.dd.plist.NSObject;
 import com.koganepj.starbuckscustomorder.model.Calorie;
+import com.koganepj.starbuckscustomorder.model.Coffee;
+import com.koganepj.starbuckscustomorder.model.Price;
+import com.koganepj.starbuckscustomorder.model.SimpleCoffeeModel;
 
 public class CalorieFinderTest extends AndroidTestCase {
 
@@ -29,6 +34,38 @@ public class CalorieFinderTest extends AndroidTestCase {
 			Log.d("test", String.format("%s のカロリーは %s です.", entry.getKey(),
 							calorie.getCalorie()));
 		}
+	}
+	
+	public void testコーヒーのサイズに合わせてカロリーを返す() {
+		// コーヒーの一覧を取得
+		CoffeeListParser parser = new CoffeeListParser(getContext());
+		ArrayList<SimpleCoffeeModel> coffeeLists = parser.getCoffeeList();
 
+		final CalorieFinder finder = new CalorieFinder(getContext());
+		
+		for (SimpleCoffeeModel model : coffeeLists) {
+			CoffeeFinder coffeeFinder = new CoffeeFinder(getContext(),
+					model.coffeeName);
+			Coffee coffee = coffeeFinder.find();
+
+			int count = coffee.size.size();
+
+			for (int i = 0; i < count; i++) {
+				// コーヒーのサイズに合わせて値段を返す
+				Calorie actualCalorie = finder.getCoffeeSizeCalorie(coffee, coffee.size.get(i));
+				
+				// 実際の値段
+				int expectedPrice = coffee.price.getPrice() + (40 * i);
+
+				String errorMessage = String.format(Locale.getDefault(),
+						"%s が見つかりません", model.coffeeName.getCoffeeName());
+
+				// 値段が合っているか確認する
+				assertEquals(errorMessage, expectedPrice,
+						actualPrice.getPrice());
+				// 一応コーヒーの名前を出力して確認しておく
+				Log.d("test", "The coffeeName is " + coffee.name);
+			}
+		}
 	}
 }
