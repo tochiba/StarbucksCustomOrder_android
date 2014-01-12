39d9c383788e4ad7c417184f8800b77d54157add
alk-tnk <alk.tnk@gmail.com>
2014-01-12 12:16:02 +0900

    メニュー画面に広告追加

--- StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/view/menu/MenuFragment.java
+++ StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/view/menu/MenuFragment.java
@@ -9,8 +9,11 @@ import android.widget.FrameLayout;
 import android.widget.RadioGroup;
 
 import com.koganepj.starbuckscustomorder.R;
+import com.koganepj.starbuckscustomorder.admob.AdmobWrapper;
 
 public class MenuFragment extends Fragment {
+	AdmobWrapper mHeaderAdWrapper;
+	AdmobWrapper mFooterAdWrapper;
     
     @Override
     public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -19,12 +22,31 @@ public class MenuFragment extends Fragment {
         FrameLayout frameLayout = (FrameLayout)view.findViewById(R.id.ListFrame);
         RadioGroup modeRadioGroup = (RadioGroup)view.findViewById(R.id.RadioGroupMenuShowType);
         
-        modeRadioGroup.setOnCheckedChangeListener(new ModeChangeListener(getActivity(), frameLayout));
+        //広告
+        mHeaderAdWrapper = new AdmobWrapper(getActivity());
+        mFooterAdWrapper = new AdmobWrapper(getActivity());
+        
+        modeRadioGroup.setOnCheckedChangeListener(new ModeChangeListener(getActivity(), frameLayout, mHeaderAdWrapper, mFooterAdWrapper));
         
         //初期表示
         modeRadioGroup.check(R.id.RadioSimple);
         
         return view;
+    }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	
+    	mHeaderAdWrapper.loadAd();
+    	mFooterAdWrapper.loadAd();
+    }
+    
+    @Override
+    public void onDestroy() {
+    	mHeaderAdWrapper.destroy();
+    	mFooterAdWrapper.destroy();
+    	super.onDestroy();
     }
     
 }
--- StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/view/menu/ModeChangeListener.java
+++ StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/view/menu/ModeChangeListener.java
@@ -10,6 +10,7 @@ import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 
 import com.koganepj.starbuckscustomorder.R;
+import com.koganepj.starbuckscustomorder.admob.AdmobWrapper;
 import com.koganepj.starbuckscustomorder.model.SimpleCoffeeModel;
 import com.koganepj.starbuckscustomorder.parse.CoffeeListParser;
 import com.koganepj.starbuckscustomorder.view.menu.adapter.MenuAdapter;
@@ -22,13 +23,17 @@ class ModeChangeListener implements OnCheckedChangeListener {
     private Context mContext;
     private ViewGroup mFrameLayout;
     private LayoutInflater mInflater;
+    private AdmobWrapper mHeaderAdWrapper;
+    private AdmobWrapper mFooterAdWrapper;
     
     private MenuAdapter mMenuAdapter;
     
-    public ModeChangeListener(Context context, ViewGroup frameLayout) {
+    public ModeChangeListener(Context context, ViewGroup frameLayout, AdmobWrapper headerAdWrapper, AdmobWrapper footerWraper) {
         mContext = context;
         mFrameLayout = frameLayout;
         mInflater = LayoutInflater.from(context);
+        mHeaderAdWrapper = headerAdWrapper;
+        mFooterAdWrapper = footerWraper;
         
         prepareAdapter();
     }
@@ -45,6 +50,10 @@ class ModeChangeListener implements OnCheckedChangeListener {
     
     @Override
     public void onCheckedChanged(RadioGroup group, int checkedId) {
+    	// 広告の更新
+    	mHeaderAdWrapper.loadAd();
+    	mFooterAdWrapper.loadAd();
+    	
         if (checkedId == R.id.RadioSimple) {//シンプルモード
             //ListViewの入れ替え
             mFrameLayout.removeAllViews();
@@ -53,7 +62,10 @@ class ModeChangeListener implements OnCheckedChangeListener {
             listView.setOnItemClickListener(new OnRowClickListener());
             
             mMenuAdapter.setModeTo(MenuMode.SIMPLE);
+            listView.addHeaderView(mHeaderAdWrapper.getAdView());
+            listView.addFooterView(mFooterAdWrapper.getAdView());
             listView.setAdapter(mMenuAdapter);
+            
             return;
         }
         
@@ -65,6 +77,8 @@ class ModeChangeListener implements OnCheckedChangeListener {
             listView.setOnItemClickListener(new OnRowClickListener());
 
             mMenuAdapter.setModeTo(MenuMode.VISUAL);
+            listView.addHeaderView(mHeaderAdWrapper.getAdView());
+            listView.addFooterView(mFooterAdWrapper.getAdView());
             listView.setAdapter(mMenuAdapter);
             return;
         }
