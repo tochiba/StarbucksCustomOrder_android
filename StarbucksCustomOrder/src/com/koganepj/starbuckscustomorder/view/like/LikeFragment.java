a28e3a5854b2f802fa862677cf21dd243d950df2
alk-tnk <alk.tnk@gmail.com>
2014-01-12 12:17:11 +0900

    お好み画面に広告追加

--- StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/view/like/LikeFragment.java
+++ StarbucksCustomOrder/src/com/koganepj/starbuckscustomorder/view/like/LikeFragment.java
@@ -5,13 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView.FindListener;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import com.koganepj.starbuckscustomorder.R;
+import com.koganepj.starbuckscustomorder.admob.AdmobWrapper;
 import com.koganepj.starbuckscustomorder.parse.CoffeeNameFinder;
 import com.koganepj.starbuckscustomorder.view.like.matrix.MatrixSelectorLayout;
 
 public class LikeFragment extends Fragment {
-    
+	private AdmobWrapper mWrapper;
+	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_like, null);
@@ -25,7 +30,23 @@ public class LikeFragment extends Fragment {
         View okButton = view.findViewById(R.id.ButtonOk);
         okButton.setOnClickListener(new OnDecideButtonClickListener(matrixSelectorLayout, finder));
         
+        // 広告
+        mWrapper = new AdmobWrapper(getActivity()); 
+        ((FrameLayout) view.findViewById(R.id.FrameAd)).addView(mWrapper.getAdView());
+        
         return view;
     }
     
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	
+        mWrapper.loadAd();
+    }
+    
+    @Override
+    public void onDestroy() {
+    	mWrapper.destroy();
+    	super.onDestroy();
+    }
 }
